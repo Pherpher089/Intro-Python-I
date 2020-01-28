2d1ec7314c90dd29b5d5beb41a0ca983be035145
Pherpher089 <Christutor089@gmail.com>
2020-01-27 18:36:50 -0800

    Corrected there being no inheritence in the class section

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('src/foo.txt') as f:
+    for i in f:
+        print(i)
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +20,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open('src/bar.txt', 'a') as f:
+    for i in range(3):
+        f.write("line number: %i \n" % (i))
+
+with open('src/bar.txt') as f:
+    for i in f:
+        print(i)
